d27f79a52cadd9bed4e5eb9bb1cad003e55a2f34
Klaudia Bronowicka <klaudia@shout-app.com>
2019-11-26 12:28:28 +0000

    Initial work on the clock face

---
+++ ClockApp/ClockHands.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
+namespace ClockApp
+{
+    public partial class ClockHands : ContentView
+    {
+        SKPaint backgroundBrush = new SKPaint()
+        {
+            Style = SKPaintStyle.Fill,
+            Color = Color.Red.ToSKColor()
+        };
+
+        public ClockHands()
+        {
+            InitializeComponent();
+            StartClock();
+        }
+
+        private void StartClock()
+        {
+
+        }
+
+        private void ClockHandsCanvas_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs e)
+        {
+            SKImageInfo info = e.Info;
+            SKSurface surface = e.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            // get density
+            float density = info.Size.Width / (float)Width;
+
+            var clockView = view
+
+            canvas.DrawRect(new SKRect(0, 0, 100, 100), backgroundBrush);
+            
+        }
+    }
+}
